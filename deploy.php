8ce7778601785d2825e32056b607c6c2f0c0b9a8
amidaniram <amidaniram@yandex.ru>
2014-09-04 01:07:11 +0400

    deploy.php v.0.9

--- deploy.php
+++ deploy.php
@@ -3,10 +3,12 @@
         if ($_POST['user'] != "")
         {
             $user = $_POST['user'];
-            echo "cd {$user}; git pull https://github.com/dotaleague/{$user}.git master 2>&1<br>";
-            echo exec("cd {$user}; git pull https://github.com/dotaleague/{$user}.git master 2>&1", $output, $return_var);
-            var_dump($return_var);
-            echo "return_var is: $return_var" . "\n";
-            var_dump($output);
-            //header("Refresh:3; url={$user}");
+            exec("cd {$user}; git pull https://github.com/dotaleaguetest/{$user}.git master");
+            /* DEBUG
+             * echo exec("cd {$user}; git pull https://github.com/dotaleaguetest/{$user}.git master 2>&1", $output, $return_var);
+             * var_dump($return_var);
+             * echo "return_var is: $return_var" . "\n";
+             * var_dump($output);
+             */
+            header("Refresh:3; url={$user}");
         }
