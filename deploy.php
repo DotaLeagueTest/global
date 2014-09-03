291b7aced81164a8f86e378584609c6e36ed30a1
amidaniram <amidaniram@yandex.ru>
2014-09-04 00:37:11 +0400

    1

--- deploy.php
+++ deploy.php
@@ -3,6 +3,6 @@
         if ($_POST['user'] != "")
         {
             $user = $_POST['user'];
-            exec("cd {$user}; git pull https://github.com/dotaleague/{$user}.git master");
+            exec("cd {$user}; git pull https://github.com/dotaleague/{$user}.git master 2>&1");
             header("Refresh:3; url={$user}");
         }
