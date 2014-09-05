a3e7fecb0b83df826c39e305376f1fcabf1fbcab
amidaniram <amidaniram@yandex.ru>
2014-09-05 16:06:21 +0400

    Added deploytest.php

---
+++ deploytest.php
@@ -0,0 +1,10 @@
+<?php
+    if (isset($_POST['user']))
+        if ($_POST['user'] != "")
+        {
+            $user = $_POST['user'];
+             echo exec("cd {$user}; git pull https://github.com/dotaleaguetest/{$user}.git master 2>&1", $output, $return_var);
+             var_dump($return_var);
+             echo "return_var is: $return_var" . "\n";
+             var_dump($output);
+        }
